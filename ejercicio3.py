c9c5b1bdab0b967449e7f2eb38eee4a28051290c
IgnaArntz <ig.arntz@duocuc.cl>
2025-06-14 11:20:51 -0400

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,15 @@
+curso =[]
+print("")
+contador = 0 
+
+nombre = ("Ingrese Nombre Completo: ")
+dirección = ("Ingrese su dirección: ")
+telefono = ("Ingrese N° Telefono: ")
+
+alumno = ("Desea agregar otro alumno?(s/n): ")
+while True:
+    if alumno == ("s"):
+        print(nombre,)
+    if(curso !=0):
+        curso.append(curso)
+
